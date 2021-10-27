044118eb62861b462f8e5a38849d54c34cad33a6
Leo Brown <leo_brown_1@hotmail.com>
2021-10-27 16:26:12 +0100

    Fixed getTasks method

--- app/src/main/java/todo/Task.java
+++ app/src/main/java/todo/Task.java
@@ -6,57 +6,56 @@ public class Task {
 
   ArrayList<Task> tasklist; 
 
-  public void Task() {
-    ArrayList<Task> tasklist = new ArrayList<Task>();
+  Task() {
+    tasklist = new ArrayList<Task>();
   }
 
-  public String addTask(String task) {
-    String 
-    tasklist.add(task);
-  }
+  // public String addTask(String task) {
+  //   String 
+  //   tasklist.add(task);
+  // }
 
   public ArrayList<Task> getTasks() {
-    System.out.println(tasklist);
-    return tasklist;
+    return this.tasklist;
   }
 
 }
 
-public class Task {
+// public class Task {
 
-  // This is a "nested" static class made for convenience here - 
-  // you can assume the following line is right!
+//   // This is a "nested" static class made for convenience here - 
+//   // you can assume the following line is right!
 
-      // Properties
-      private String taskItem;
+//       // Properties
+//       private String taskItem;
 
-      // Constructor
-      public Student(String taskItem) {
-          this.taskItem = taskItem;
-      } 
+//       // Constructor
+//       public Student(String taskItem) {
+//           this.taskItem = taskItem;
+//       } 
 
-      // Getters
-      public String getName() {
-          return this.name;
-      }
+//       // Getters
+//       public String getName() {
+//           return this.name;
+//       }
 
-      public String getCohortName() {
-          return this.cohortName;
-      }
+//       public String getCohortName() {
+//           return this.cohortName;
+//       }
 
-  public static void main(String[] args) {
-      ArrayList<StudentDirectory.Student> studentList = new ArrayList<StudentDirectory.Student>();
-      studentList.add(new Student("Alex", "January 2020"));
-      studentList.add(new Student("Maria", "January 2020"));
-      studentList.add(new Student("Lisa", "January 2020"));
+//   public static void main(String[] args) {
+//       ArrayList<StudentDirectory.Student> studentList = new ArrayList<StudentDirectory.Student>();
+//       studentList.add(new Student("Alex", "January 2020"));
+//       studentList.add(new Student("Maria", "January 2020"));
+//       studentList.add(new Student("Lisa", "January 2020"));
 
-      Enumeration<Student> enumeration = Collections.enumeration(studentList);
+//       Enumeration<Student> enumeration = Collections.enumeration(studentList);
 
-      System.out.println("List of students:");
-      while (enumeration.hasMoreElements()) {
-          Student currentStudent = enumeration.nextElement();
-          String line = String.format(" * %s - %s", currentStudent.name, currentStudent.cohortName);
-          System.out.println(line);
-      }
-  }
-}
+//       System.out.println("List of students:");
+//       while (enumeration.hasMoreElements()) {
+//           Student currentStudent = enumeration.nextElement();
+//           String line = String.format(" * %s - %s", currentStudent.name, currentStudent.cohortName);
+//           System.out.println(line);
+//       }
+//   }
+// }
--- app/src/test/java/todo/TaskTest.java
+++ app/src/test/java/todo/TaskTest.java
@@ -10,6 +10,6 @@ public class TaskTest {
     @Test public void testStartWithEmptyTodo() {
         Task classUnderTest = new Task();
         ArrayList<Task> emptytest = new ArrayList<Task>();
-        assertEquals("check list starts empty", null, classUnderTest.getTasks());
+        assertEquals("check list starts empty", classUnderTest.getTasks(), emptytest);
     }
 }
